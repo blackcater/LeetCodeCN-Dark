cef9337f15db5b4c5b13cea81bc41209b7c38cae
Elon Tang <blackcater2015@gmail.com>
2020-08-13 21:47:40 +0800

    feat: add some less file

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,22 @@
-module.exports = (gulp) => {
-  gulp.registerTask("default", []);
-};
+const { task, watch, series, src, dest } = require("gulp");
+const less = require("gulp-less");
+const concat = require("gulp-concat");
+const LessPluginInlineSvg = require("less-plugin-inline-svg");
+const LessPluginAutoPrefix = require("less-plugin-autoprefix");
+const pkg = require("./package.json");
+
+function buildLess() {
+  return src("src/*.less")
+    .pipe(
+      less({
+        plugins: [
+          new LessPluginAutoPrefix({ browsers: ["last 2 versions", "> 1%"] }),
+          new LessPluginInlineSvg({ base64: true }),
+        ],
+      })
+    )
+    .pipe(concat("leetcode-cn-dark.user.css"))
+    .pipe(dest("./"));
+}
+
+task("build", series(buildLess));
